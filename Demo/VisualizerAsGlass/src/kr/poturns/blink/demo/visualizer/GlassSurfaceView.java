a2abce2f23a585943902665f205b382e3a280028
Jiwon <kjo6152@naver.com>
2014-11-07 18:55:27 +0900

    Glass앱에서 사진찍기 요청에 중복해서 올 경우 동기화 처리

--- Demo/VisualizerAsGlass/src/kr/poturns/blink/demo/visualizer/GlassSurfaceView.java
+++ Demo/VisualizerAsGlass/src/kr/poturns/blink/demo/visualizer/GlassSurfaceView.java
@@ -18,21 +18,23 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.widget.Toast;
 
-public class GlassSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+public class GlassSurfaceView extends SurfaceView implements
+        SurfaceHolder.Callback {
 
 	private SurfaceHolder mSurfaceHolder;
 	private Camera mCamera;
 	private Context context;
 	private PhotoHandler mPhotoHandler;
+	private boolean isTakingPicture = false;
 	
 	public GlassSurfaceView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		
+
 		mSurfaceHolder = getHolder();
 		mSurfaceHolder.addCallback(this);
 		this.context = context;
 		mPhotoHandler = new PhotoHandler(context);
-		//mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		// mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
 	}
 
 	@Override
@@ -40,7 +42,7 @@ public class GlassSurfaceView extends SurfaceView implements SurfaceHolder.Callb
 		try {
 			mCamera = Camera.open();
 			mCamera.setPreviewDisplay(mSurfaceHolder);
-			
+
 		} catch (Exception e) {
 			if (mCamera != null) {
 				mCamera.release();
@@ -50,12 +52,13 @@ public class GlassSurfaceView extends SurfaceView implements SurfaceHolder.Callb
 	}
 
 	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+	        int height) {
 		if (mCamera == null) {
 			try {
 				mCamera = Camera.open();
 				mCamera.setPreviewDisplay(mSurfaceHolder);
-				
+
 			} catch (Exception e) {
 				if (mCamera != null) {
 					mCamera.release();
@@ -63,24 +66,24 @@ public class GlassSurfaceView extends SurfaceView implements SurfaceHolder.Callb
 				}
 			}
 		}
-		
+
 		if (mCamera == null)
 			return;
-		
+
 		Camera.Parameters mParameters = mCamera.getParameters();
-		
+
 		List<Size> mPreviewSizeList = mParameters.getSupportedPreviewSizes();
-		
-		if (mPreviewSizeList == null){
+
+		if (mPreviewSizeList == null) {
 			mParameters.setPreviewSize(width, height);
-			
-			Log.i("tag", "width:"+width+"height:"+height);
-			
+
+			Log.i("tag", "width:" + width + "height:" + height);
+
 		} else {
 			int diff = 10000;
 			Size opti = null;
-			for(Size s : mPreviewSizeList){
-				if(Math.abs(s.height - height) < diff) {
+			for (Size s : mPreviewSizeList) {
+				if (Math.abs(s.height - height) < diff) {
 					diff = Math.abs(s.height - height);
 					opti = s;
 				}
@@ -96,84 +99,102 @@ public class GlassSurfaceView extends SurfaceView implements SurfaceHolder.Callb
 		if (mCamera != null) {
 			mCamera.stopPreview();
 			mCamera.release();
-	        mCamera = null;
+			mCamera = null;
 		}
 	}
-	
-	public void lightOn(){
-		if(mCamera==null)return;
+
+	public void lightOn() {
+		if (mCamera == null)
+			return;
 		Camera.Parameters mCameraParameter = mCamera.getParameters();
 		List<String> FlashModeList = mCameraParameter.getSupportedFlashModes();
-		if(FlashModeList.contains(Parameters.FLASH_MODE_TORCH)){
+		if (FlashModeList.contains(Parameters.FLASH_MODE_TORCH)) {
 			mCameraParameter.setFlashMode(Parameters.FLASH_MODE_TORCH);
 			mCamera.setParameters(mCameraParameter);
-		}
-		else {
-			Toast.makeText(context, "플래시를 켤 수 없습니다.", Toast.LENGTH_SHORT).show();
+		} else {
+			Toast.makeText(context, "플래시를 켤 수 없습니다.", Toast.LENGTH_SHORT)
+			        .show();
 		}
 	}
-	
-	public void lightOff(){
-		if(mCamera==null)return;
+
+	public void lightOff() {
+		if (mCamera == null)
+			return;
 		Camera.Parameters mCameraParameter = mCamera.getParameters();
 		List<String> FlashModeList = mCameraParameter.getSupportedFlashModes();
-		if(FlashModeList.contains(Parameters.FLASH_MODE_OFF)){
+		if (FlashModeList.contains(Parameters.FLASH_MODE_OFF)) {
 			mCameraParameter.setFlashMode(Parameters.FLASH_MODE_OFF);
 			mCamera.setParameters(mCameraParameter);
+		} else {
+			Toast.makeText(context, "플래시를 끌 수 없습니다.", Toast.LENGTH_SHORT)
+			        .show();
+		}
+
+	}
+
+	
+	public synchronized boolean lockTakePicture(){
+		if(isTakingPicture){
+			return false;
 		}
 		else {
-			Toast.makeText(context, "플래시를 끌 수 없습니다.", Toast.LENGTH_SHORT).show();
+			isTakingPicture=true;
 		}
-		
+		return true;
 	}
 	
-	public void takePicture(){
-		if(mCamera==null)return;
-		mCamera.takePicture(null, null, mPhotoHandler);
+	public void takePicture() {
+		if (mCamera == null)
+			return;
+		if(lockTakePicture())mCamera.takePicture(null, null, mPhotoHandler);
 	}
-	
+
 	class PhotoHandler implements PictureCallback {
 
-		  private final Context context;
+		private final Context context;
 
-		  public PhotoHandler(Context context) {
-		    this.context = context;
-		  }
+		public PhotoHandler(Context context) {
+			this.context = context;
+		}
 
-		  @Override
-		  public void onPictureTaken(byte[] data, Camera camera) {
+		@Override
+		public void onPictureTaken(byte[] data, Camera camera) {
+			File pictureFileDir = getDir();
 
-		    File pictureFileDir = getDir();
+			if (!pictureFileDir.exists() && !pictureFileDir.mkdirs()) {
+				Toast.makeText(context, "사진을 저장할 수 없습니다.", Toast.LENGTH_LONG)
+				        .show();
+				return;
 
-		    if (!pictureFileDir.exists() && !pictureFileDir.mkdirs()) {
-		      Toast.makeText(context, "사진을 저장할 수 없습니다.",Toast.LENGTH_LONG).show();
-		      return;
+			}
 
-		    }
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyymmddhhmmss");
+			String date = dateFormat.format(new Date());
+			String photoFile = "Picture_" + date + ".jpg";
+			String filename = pictureFileDir.getPath() + File.separator
+			        + photoFile;
 
-		    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyymmddhhmmss");
-		    String date = dateFormat.format(new Date());
-		    String photoFile = "Picture_" + date + ".jpg";
-		    String filename = pictureFileDir.getPath() + File.separator + photoFile;
+			File pictureFile = new File(filename);
 
-		    File pictureFile = new File(filename);
+			try {
+				FileOutputStream fos = new FileOutputStream(pictureFile);
+				fos.write(data);
+				fos.close();
+				Toast.makeText(context, "사진이 저장되었습니다.", Toast.LENGTH_LONG)
+				        .show();
+			} catch (Exception error) {
+				Toast.makeText(context, "사진을 저장할 수 없습니다.", Toast.LENGTH_LONG)
+				        .show();
+			}
 
-		    try {
-		      FileOutputStream fos = new FileOutputStream(pictureFile);
-		      fos.write(data);
-		      fos.close();
-		      Toast.makeText(context, "사진이 저장되었습니다.",Toast.LENGTH_LONG).show();
-		    } catch (Exception error) {
-		      Toast.makeText(context, "사진을 저장할 수 없습니다.",Toast.LENGTH_LONG).show();
-		    }
-		    
-		    camera.startPreview();
-		  }
+			camera.startPreview();
+			isTakingPicture = false;
+		}
 
-		  private File getDir() {
-		    File sdDir = Environment
-		      .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-		    return new File(sdDir, "VisualizerAsGlass");
-		  }
-		} 
+		private File getDir() {
+			File sdDir = Environment
+			        .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+			return new File(sdDir, "VisualizerAsGlass");
+		}
+	}
 }
